1c37997e7eb9f758dd28d25e3c134ef54296ae8b
francesco-cmcc <62609884+francesco-cmcc@users.noreply.github.com>
2021-09-14 10:25:30 +0200

    Update convert_1year.sh
    Also LAND_SEA_MASK path is required

--- tests/convert_1year.sh
+++ tests/convert_1year.sh
@@ -3,6 +3,7 @@
 export ATMDATA=/work/remhi/pm09220/medcordex/from_cmip5_global/CNRM-CM5/historical/r1i1p1/result/atm_regional/2000
 export OCNDATA=/work/remhi/pm09220/medcordex/from_cmip5_global/CNRM-CM5/historical/r1i1p1/result/oce_regional/2000
 export OUTPATH=../intermediate_out
+export LAND_MASK_PATH=/work/remhi/pm09220/medcordex/mask_laurent/phis_box.nc
 mkdir -p $OUTPATH
 module load ncl
 ncl ../src/mcordex2int.ncl
